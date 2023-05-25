af2090f8924c9354d2af15f67054c395a7716235
RamyBadr1 <ramybadr054@gmail.com>
2023-05-25 16:41:52 +0300

    _getline.c

--- _getline.c
+++ _getline.c
@@ -1,55 +1,51 @@
 #include "main.h"
 
 /**
- * _getline - gets a input from STDIN
- * @info: parameter struct
- * @pointer: address of pointer
+ * _getline - gets input from STDIN
+ * @info: struct
+ * @pointer: address of ptr to buffer
  * @length: length of ptr buffer
- * Return: s
+ * Return: size_t value
  */
 int _getline(info_t *info, char **pointer, size_t *length)
 {
-	static char buffer[READ_BUF_SIZE];
-	static size_t index; 
-	size_t len;
+	static char buf[READ_BUF_SIZE];
+	static size_t i;
+	static size_t len;
 	size_t k;
-	size_t read = 0; 
-	size_t s = 0;
-	char *p = NULL; 
+	ssize_t read = 0;
+	ssize_t s = 0;
+	char *p = NULL;
 	char *new_p = NULL;
-	char *character;
+	char *c;
 
 	p = *pointer;
 	if (p != NULL && length != NULL)
 		s = *length;
-	if (index == len)
-		index = len = 0;
+	if (i == len)
+		i = len = 0;
 
-	read = read_buf(info, buffer, &len);
-
+	read = read_buf(info, buf, &len);
 	if (read < 0 || (read == 0 && len == 0))
 		return (-1);
 
-	character = _strchr(buffer + index, '\n');
-	k = character ? 1 + (unsigned int)(character - buffer) : len;
+	c = _strchr(buf + i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
 	new_p = _realloc(p, s, s ? s + k : k + 1);
-
-	if (new_p = NULL) /* MALLOC FAILURE! */
+	if (new_p == NULL) /* MALLOC FAILURE! */
 		return (p ? free(p), -1 : -1);
 
-	if (s)
-		_strncat(new_p, buffer + index, k - index);
+	if (s != 0)
+		_strncat(new_p, buf + i, k - i);
 	else
-		_strncpy(new_p, buffer + index, k - index + 1);
+		_strncpy(new_p, buf + i, k - i + 1);
 
-	s += k - index;
-	index = k;
+	s += k - i;
+	i = k;
 	p = new_p;
 
-	if (length != NULL)
+	if (length)
 		*length = s;
-
 	*pointer = p;
-	
 	return (s);
 }
